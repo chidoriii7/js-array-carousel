d557f85caec97642bd48464252e25f897db89314
chidoriii7 <muratore.danilo@yahoo.it>
2024-03-15 10:10:24 +0100

    carousel - passaggio classe active

--- app.js
+++ app.js
@@ -3,28 +3,46 @@ const imagesArray = [
     ' img/02.webp ',
     ' img/03.webp ',
     ' img/04.webp ',
-    ' img/05.webp '
+    ' img/05.webp ',
 ]
 
 //arrivare a list-item
 const itemListDOMElement = document.querySelector('.list-item');
+const nextButton = document.querySelector('next')
+const prevButton = document.querySelector('prev')
 
 //le immagini sono già inserite
 
 //far comparire la prima immagine (active)
 //far slidare le immagini seguenti
-for (let i = 0; imagesArray.length; i++) {
-    
+for (let i = 0; i < imagesArray.length; i++) {
+
     const imageURL = imagesArray[i]
+    
+    let classImage = 'immagine attiva'
+//partendo dalla prima slide do la classe "active"
+    if (i === 0) {
+        classImage = ' active'
+    }
 
     const itemsHTML = 
 
     `
-        <div class="item">
+        <div class="item ${classImage}">
             <img src="${ imageURL }">
         </div>
     `;
 
-    console.log(itemsHTML);
-    
 }
+
+
+//click sul pulsante next per prossima slide
+nextButton.addEventListener('click', function() {
+
+})
+//click su pulsante prev per slide precedente
+
+prevButton.addEventListener('click', function() {
+
+})
+
